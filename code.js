653b3c35136ab93256e46fea7b5ce92ab83944ab
Oscar Nunez <noscar07@gmail.com>
2020-01-28 23:45:46 -0500

    added html elements and added psuedo code

---
+++ code.js
@@ -0,0 +1,27 @@
+/* 
+psuedo code
+customer will add the total bill in the text input(I have to make sure that customer can only add numbers)
+customer will then chose the service of the server. 
+customer will then type how many guest they will want to split the check with(just like the total bill I need to limit my customer to only type number)
+
+when customer hits submit we need to display 
+the new bill with the tip included
+and if the customer added other people to split check how much it will be for all of them.
+
+I need to keep track of the total bill my customer added 
+
+I need to keep track of what service my customer picked 
+
+I need to keep track if my customer added any person to split the bill
+
+I need to keep track of when the customer hit submit
+
+*/
+
+var totalBill= ""
+
+var service = ""
+
+var splitCheck = ""
+
+var finalBill
